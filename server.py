70f580f06160809bd671b6c8a31d00b58770d722
theydvgaurav <gauravyadav2609@gmail.com>
2024-01-24 17:44:29 +0530

    refactor: removed print statement and python run statement

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ from base.exception_handler.exception_handler import error_handler_blueprint
 from common.logging.logger import JournyxLogger
 
 config_path = Path(__file__).with_name("logging_conf.json")
-print(config_path)
 journyx_logger = JournyxLogger.make_logger(config_path)
 
 app = Flask(__name__)
@@ -18,6 +17,3 @@ CORS(app)
 app.register_blueprint(error_handler_blueprint)
 app.register_blueprint(diary_blueprint, url_prefix='/v1')
 app.register_blueprint(users_blueprint, url_prefix='/v1')
-
-if __name__ == '__main__':
-    app.run(port=4070, debug=True)
